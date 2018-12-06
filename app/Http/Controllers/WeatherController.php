51aa9d8f2c16a06c6b8ef8205394c7d20765c1c1
Milan V <milvuk@gmail.com>
2018-12-06 17:14:02 +0100

    Getting and caching geolocation data

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use \OpenCage\Geocoder\Geocoder;
+use App\Retrievers\GeoRetriever;
 
 class WeatherController extends Controller
 {
     public function index()
     {
-        $geocoder = new Geocoder('c0786d1b1f034b4b8b9e75b78aa92d0f');
-        $result = $geocoder->geocode('Banja Luka');
-        var_dump($result);
-        //echo 'tu smo!';
+        $geoRetriever = new GeoRetriever();
 
+        $rez = $geoRetriever->getGeoLocation('Banja Luka');
+
+        var_dump($rez);
     }
 }
---
+++ app/Retrievers/GeoRetriever.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Retrievers;
+
+use OpenCage\Geocoder\Geocoder;
+use Illuminate\Support\Facades\Cache;
+
+class GeoRetriever
+{
+    private $geoCoder;
+    private $apiKey = 'c0786d1b1f034b4b8b9e75b78aa92d0f';
+    private $cacheValidityPeriod = 60 * 24 * 30;                // Minutes data in cache considered valid
+
+    public function __construct()
+    {
+        $this->geoCoder = new Geocoder($this->apiKey);
+    }
+
+    public function getGeoLocation($address)
+    {
+        $address = trim($address);
+
+        // Use cached data if we already have it
+        $geoData = Cache::get('geo-data-' . $address);
+
+        // If not, get fresh data from API
+        if (!$geoData) {
+            $geoData = $this->doGetGeoLocation($address);
+            if ($geoData) {
+                // Store fresh data to cache
+                Cache::put('geo-data-' . $address, $geoData, $this->cacheValidityPeriod);
+            }
+        }
+
+        return $geoData;
+    }
+
+    private function doGetGeoLocation($address)
+    {
+        try {
+            $result = $this->geoCoder->geocode($address);
+            if ($result && $result['total_results'] > 0) {
+                $firstResult = $result['results'][0];
+                return [
+                    'lat'           => $firstResult['geometry']['lat'],
+                    'lon'           => $firstResult['geometry']['lng'],
+                    'description'   => $firstResult['formatted']
+                ];
+            }
+        } catch (\Exception $e) {
+
+        }
+        return false;
+    }
+}
