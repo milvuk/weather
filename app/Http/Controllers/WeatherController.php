a73a9e5ffb87b042d34781266867a32025494aa3
Milan V <milvuk@gmail.com>
2018-12-06 16:18:46 +0100

    composer: add opencage geocoder package

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use \OpenCage\Geocoder\Geocoder;
+
 class WeatherController extends Controller
 {
     public function index()
     {
-        echo 'tu smo!';
+        $geocoder = new Geocoder('c0786d1b1f034b4b8b9e75b78aa92d0f');
+        $result = $geocoder->geocode('Banja Luka');
+        var_dump($result);
+        //echo 'tu smo!';
+
     }
 }
